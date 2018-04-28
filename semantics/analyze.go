e7fa3987c592279e87f7880f25ce303d10ae276d
Niklas Korz <korz.niklask@gmail.com>
2018-04-28 14:17:44 +0200

    Move definition scope to separate file

--- semantics/analyze.go
+++ semantics/analyze.go
@@ -6,39 +6,6 @@ import (
 	"niklaskorz.de/nklang/ast"
 )
 
-type definitionSet map[string]struct{}
-
-func (s definitionSet) set(name string) {
-	s[name] = struct{}{}
-}
-
-func (s definitionSet) has(name string) bool {
-	_, ok := s[name]
-	return ok
-}
-
-type definitionScope struct {
-	parent      *definitionScope
-	definitions definitionSet
-}
-
-func (scope *definitionScope) newScope() *definitionScope {
-	return &definitionScope{
-		parent:      scope,
-		definitions: make(definitionSet),
-	}
-}
-
-func (scope *definitionScope) lookup(name string, index int) int {
-	if scope.definitions.has(name) {
-		return index
-	}
-	if scope.parent == nil {
-		return -1
-	}
-	return scope.parent.lookup(name, index+1)
-}
-
 func AnalyzeLookups(p *ast.Program) error {
 	globalScope := &definitionScope{definitions: make(definitionSet)}
 
---
+++ semantics/definition_scope.go
@@ -0,0 +1,34 @@
+package semantics
+
+type definitionSet map[string]struct{}
+
+func (s definitionSet) set(name string) {
+	s[name] = struct{}{}
+}
+
+func (s definitionSet) has(name string) bool {
+	_, ok := s[name]
+	return ok
+}
+
+type definitionScope struct {
+	parent      *definitionScope
+	definitions definitionSet
+}
+
+func (scope *definitionScope) newScope() *definitionScope {
+	return &definitionScope{
+		parent:      scope,
+		definitions: make(definitionSet),
+	}
+}
+
+func (scope *definitionScope) lookup(name string, index int) int {
+	if scope.definitions.has(name) {
+		return index
+	}
+	if scope.parent == nil {
+		return -1
+	}
+	return scope.parent.lookup(name, index+1)
+}
